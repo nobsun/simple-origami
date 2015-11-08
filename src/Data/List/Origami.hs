bce820d6d13f8847b88e6c9500da88af9817ab70
Nobuo Yamashita <nobsun@sampou.org>
2015-11-08 18:25:52 +0900

    modify api for ana and hylo

--- src/Data/List/Origami.hs
+++ src/Data/List/Origami.hs
@@ -4,20 +4,18 @@ cata :: b -> (a -> b -> b) -> [a] -> b
 cata b _ []     = b
 cata b f (x:xs) = f x (cata b f xs)
 
-ana :: (b -> (a,b)) -> (b -> Bool) -> b -> [a]
-ana g p b | p b       = []
-          | otherwise = a : ana g p b'
-                        where
-                          (a,b') = g b
+ana :: (b -> Maybe (a,b)) -> b -> [a]
+ana g b = case g b of
+  Nothing     -> []
+  Just (a,b') -> a : ana g b'
 
-hylo :: c -> (b -> c -> c) -> (a -> (b,a)) -> (a -> Bool) -> a -> c
-hylo c f g p a | p a       = c
-               | otherwise = f b (hylo c f g p a')
-                             where
-                               (b,a') = g a
+hylo :: c -> (b -> c -> c) -> (a -> Maybe (b,a)) -> a -> c
+hylo c f g a = case g a of
+  Nothing     -> c
+  Just (b,a') -> f b (hylo c f g a')
 
 -- hylo c f g p a = cat c f . ana g p a
 
 para :: b -> (a -> ([a],b) -> b) -> [a] -> b
-para b f []     = b
+para b _ []     = b
 para b f (a:as) = f a (as, para b f as)
